2ab9add057ea6d9f38595a17b6182eaaee7189a3
sonic-nick <skypotatoe22@gmail.com>
2020-02-13 14:02:25 -0500

    board looper done trying to create paint component, but i think the square class has to give the x and y locations of the squares

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 
 public class Board {
 
@@ -19,9 +20,24 @@ public class Board {
         }
     }
 
-    private void solveBoard () { //loop through board and use fill possibles on each
+    //loop through board and use fill possibles on each
+    private void solveBoard () {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                board[r][c].fillPossibles(board);
+            }
+
+        }
 
     }
 
     //draw board?
+    public void paintComponent(Graphics2D g2d){
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                g2d.drawRect();
+            }
+
+        }
+    }
 }
